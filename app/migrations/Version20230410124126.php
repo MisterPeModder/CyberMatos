969b41af10f03e63fabec7a9e4906c1d4654644c
metalbrackets <sandrine.le-levier@epitech.eu>
2023-04-10 14:44:22 +0200

    chore: create mariaDB database + connection in .env

--- app/migrations/Version20230410124126.php
+++ app/migrations/Version20230410124126.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230410114704 extends AbstractMigration
+final class Version20230410124126 extends AbstractMigration
 {
     public function getDescription(): string
     {
