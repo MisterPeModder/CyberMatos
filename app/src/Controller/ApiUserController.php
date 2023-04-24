2cfe5da826893fe5e17ad20c835afdc081bb2e5d
Yanis Guaye <yanis.guaye@epitech.eu>
2023-04-24 18:04:52 +0200

    Add the `/api/login` route
    Closes #16

--- app/src/Controller/ApiUserController.php
+++ app/src/Controller/ApiUserController.php
@@ -3,14 +3,19 @@
 namespace App\Controller;
 
 use App\Component\JsonErrorResponse;
+use App\Entity\AccessToken;
 use App\Entity\User;
+use App\Repository\AccessTokenRepository;
 use App\Repository\UserRepository;
+use App\Validator\ActuallyNotBlank;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\Validator\Constraints as Assert;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 #[Route('/api')]
@@ -39,5 +44,43 @@ class ApiUserController extends AbstractController
         $userRepository->save($newUser, true);
 
         return new JsonResponse(null, 201);
+    }
+
+    /**
+     * @throws \Exception
+     */
+    #[Route('/login', name: 'login', methods: ['POST'], condition: "request.headers.get('Content-Type') === 'application/json'")]
+    public function login(Request $request, UserPasswordHasherInterface $passwordHasher, ValidatorInterface $validator, UserRepository $userRepository, AccessTokenRepository $accessTokenRepository): JsonResponse
+    {
+        try {
+            $login = $request->get('login');
+            $password = $request->get('password');
+        } catch (\Exception $exception) {
+            return new JsonErrorResponse($exception);
+        }
+
+        $errors = $validator->validate([
+            'login' => $login,
+            'password' => $password,
+        ], new Assert\Collection([
+            'login' => new ActuallyNotBlank(),
+            'password' => new ActuallyNotBlank(),
+        ]));
+
+        if (count($errors) > 0) {
+            return new JsonErrorResponse((string) $errors);
+        }
+        $user = $userRepository->findOneBy(['login' => $login]);
+
+        if (null == $user || !$passwordHasher->isPasswordValid($user, $password)) {
+            return new JsonErrorResponse('Invalid credentials', Response::HTTP_FORBIDDEN);
+        }
+
+        $accessToken = AccessToken::generate($user);
+        $accessTokenRepository->save($accessToken, true);
+
+        return new JsonResponse([
+            'token' => $accessToken->getValue(),
+        ], 200);
     }
 }
